c8187c166e9fb207feb1beedf88a46a6077a1976
SravyaChitipotu <sravyachitipotu06@gmail.com>
2023-07-17 15:33:01 +0530

    QuizApp

---
+++ lib/data/questions.dart
@@ -0,0 +1,54 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+
+const questions = [
+   QuizQuestion(
+    'Who is the President of India?',
+    ['Droupadi Murmu', 'Ramnath Kovind', 'Pranam Mukharji', 'Narendra Modi'],
+  ),
+
+   QuizQuestion(
+    'Which Indian city is known as the "Pink City"?',
+    ['Jaipur', 'Mumbai', 'Delhi', 'Kolkata'],
+  ),
+
+   QuizQuestion(
+    'Which Indian city hosted the 2023 Indian Premier League (IPL) cricket tournament?',
+    ['Ahmedabad', 'Chennai', 'Mumbai', 'Delhi'],
+  ),
+
+  QuizQuestion(
+    'Who is the current President of the United States?',
+    ['Joe Biden', 'Donald Trump', 'Barack Obama', 'Kamala Harris'],
+  ),
+
+  QuizQuestion(
+    'Which Indian bank recently became the first to fully own a general insurance company?',
+    ['Kotak Mahindra Bank', 'Axis Bank', 'ICICI Bank', 'HDFC Bank'],
+  ),
+
+  QuizQuestion(
+    'Which Indian state is famous for its backwaters and houseboat tourism?',
+    ['Kerala', 'Goa', 'Rajasthan', 'Tamil Nadu'],
+  ),
+
+  QuizQuestion(
+    'Who is known as the "Father of the Nation" in India?',
+    ['Mahatma Gandhi', 'Jawaharlal Nehru', 'Subhash Chandra Bose', 'Bhagat Singh'],
+  ),
+
+   QuizQuestion(
+    'Which Indian festival is known as the "Festival of Lights"?',
+    ['Diwali', 'Holi', 'Navratri', 'Eid'],
+  ),
+
+   QuizQuestion(
+    'Which Indian state is famous for its tea plantations and is known as the "Tea Capital of India"?',
+    ['Assam', 'Tamil Nadu', 'Himachal Pradesh', 'Uttarakhand'],
+  ),
+
+   QuizQuestion(
+    'Who is the current Chief Minister of Delhi?',
+    ['Arvind Kejriwal', 'Manohar Lal Khattar', 'Yogi Adityanath', 'Mamata Banerjee'],
+  ),
+];
